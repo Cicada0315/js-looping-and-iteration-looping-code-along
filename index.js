41b311bac2b364d4a4fd400ac294c242afde4af1
jean <jin.ahn.1027@gmail.com>
2021-05-03 16:47:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(arr, str){
+    let thankyou=[]
+    for(let i=0; i< arr.length; i=i+1){
+        thankyou.push(("Thank you, "+ arr[i]+", for the wonderful "+ str+" gift!"));
+    }
+    return thankyou;
+}
+
+/*function writeCards(arr, str){
+    let i=0;
+    let thankyou=[]
+    while(i<arr.length){
+        thankyou.push(("Thank you, "+ arr[i]+", for the wonderful "+ str+" gift!"));
+        i=i+1;
+    }
+    return thankyou;
+}*/
+
+
+function countDown(number){
+    for(let i=number; i>=0; i=i-1){
+        
+        console.log(i);
+    }
+}
